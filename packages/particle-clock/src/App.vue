1ff93881778e1d11601dc584041b21748547d747
BOOMzit <wf15zxb@163.com>
2023-09-27 23:28:05 +0800

    chore:增加规范校验

--- packages/particle-clock/src/App.vue
+++ packages/particle-clock/src/App.vue
@@ -1,15 +1,57 @@
 <script setup>
-import { onMounted, ref } from 'vue'
-const canvas = ref()
-const ctx = canvas.value.getContext('2d')
+import { onMounted, ref } from 'vue';
+import Particle from './components/particle';
+const canvas = ref();
+const _canvas = canvas.value;
 onMounted(() => {
-  canvas.value.width = window.innerWidth * window.devicePixelRatio
-  canvas.value.height = window.innerHeight * window.devicePixelRatio
-})
+  canvas.value.width = window.innerWidth * window.devicePixelRatio;
+  canvas.value.height = window.innerHeight * window.devicePixelRatio;
+  const ctx = canvas.value.getContext('2d');
+  console.log('ctx', ctx);
+
+  const particles = [];
+  for (let i = 0; i <= 100; i++) {
+    particles.push(new Particle(canvas.value));
+  }
+  draw(ctx, particles, canvas.value);
+});
+const text = null;
+const draw = (ctx, particles, canvas) => {
+  // clear();
+  update(text, ctx, canvas);
+  particles.forEach((p) => p.draw(ctx));
+  window.requestAnimationFrame(draw);
+};
+
+const getText = () => {
+  return new Date().toTimeString().substring(0, 8);
+};
+
+const clear = (ctx) => {
+  ctx.clearRect(0, 0, canvas.value.width, canvas.value.height);
+};
+
+const update = (text, ctx, canvas) => {
+  const newText = getText();
+  if (newText === text) {
+    return;
+  }
+  clear(ctx);
+  text = newText;
+  const { width, height } = canvas;
+  ctx.fillStyle = '#000';
+  ctx.textBaseline = 'middle';
+  ctx.font = `${140 * devicePixelRatio}px 'DS-Digital,sans-serif'`;
+  ctx.fillText(text, (width - ctx.measureText(text).width) / 2, height / 2);
+};
 </script>
 
 <template>
   <canvas ref="canvas"></canvas>
 </template>
 
-<style scoped></style>
+<style scoped>
+canvas {
+  border: 1px solid;
+}
+</style>
--- packages/particle-clock/src/components/particle.js
+++ packages/particle-clock/src/components/particle.js
@@ -1,11 +1,43 @@
 const getRandom = (min, max) => {
-    return Math.floor(Math.random() * (max + 1 - min) + min)
-}
+  return Math.floor(Math.random() * (max + 1 - min) + min);
+};
 
 export default class Particle {
-    constructor() {
-        const r = Math.min(canvas.width, canvas.height) / 2
-        const cx = canvas.width / 2
-        const cy = canvas.height / 2
-    }
+  constructor(canvas) {
+    const { devicePixelRatio } = window;
+    const r = Math.min(canvas.width, canvas.height) / 2;
+    const cx = canvas.width / 2;
+    const cy = canvas.height / 2;
+    const rad = (getRandom(0, 360) * Math.PI) / 180;
+    this.x = cx + r * Math.cos(rad);
+    this.y = cy + r * Math.sin(rad);
+    this.size = getRandom(2, 5) * devicePixelRatio;
+  }
+
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.fillStyle = '#5445544d';
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  moveTo(tx, ty) {
+    const duration = 500; //500ms的运动时间
+    const sx = this.x;
+    const sy = this.y;
+    const xSpeed = (tx - sx) / duration;
+    const ySpeed = (ty - sy) / duration;
+    const startTime = Date.now();
+    const _moveTo = () => {
+      const t = Date.now() - startTime;
+      this.x = xSpeed * t;
+      this.y = ySpeed * t;
+      if (t > duration) {
+        this.x = tx;
+        this.y = ty;
+        return;
+      }
+      window.requestAnimationFrame(_moveTo);
+    };
+  }
 }
